1ea6585ef0d3739331c4bbbc43118b056394ac55
Simston <stephane.simon51@gmail.com>
2023-07-25 23:22:40 +0200

    Update Labyrinth

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
+const myGame = document.querySelector('#game');
+const alert = document.querySelector('.alert');
+
 let nbRow = 4;
 let nbCol = 4;
 let posPlayer = [0, 0];
-
-const myGame = document.querySelector('#game');
-/*
-let line1 = [createCell(5), createCell(10), createCell(1), createCell(7)];
-let line2 = [createCell(5), createCell(14), createCell(5), createCell(5)];
-let line3 = [createCell(4), createCell(1), createCell(8), createCell(5)];
-let line4 = [createCell(14), createCell(9), createCell(12), createCell(9)];*/
-let line1 = [5, 10, 1, 7]
-let line2 = [5, 14, 5, 5];
-let line3 = [4, 1, 8, 5];
-let line4 = [14, 9, 12, 9];
-let gameArray = [line1, line2, line3, line4];
+let niveauEnCours = 1;
 
 const createCell = picture => {
     let cell = {
         picture: picture,
-        left: getLeft(),
-        right: getRight(),
-        top: getTop(),
-        bot: getBot()
+        left: getLeft(picture),
+        right: getRight(picture),
+        top: getTop(picture),
+        bot: getBot(picture)
     }
     return cell
-
 }
+
 const getLeft = (picture) => {
     if (picture === 0 || picture === 1 || picture === 2 || picture === 3 || picture === 6 || picture === 7 || picture === 8 || picture === 12) {
         return true;
@@ -45,7 +37,11 @@ const getBot = (picture) => {
         return true;
     }
 }
-
+let line1 = [createCell(5), createCell(10), createCell(1), createCell(7)];
+let line2 = [createCell(5), createCell(14), createCell(5), createCell(5)];
+let line3 = [createCell(4), createCell(1), createCell(8), createCell(5)];
+let line4 = [createCell(14), createCell(9), createCell(12), createCell(9)];
+let gameArray = [line1, line2, line3, line4];
 
 const displayLabyrinth = array => {
     myGame.innerHTML = "";
@@ -54,11 +50,12 @@ const displayLabyrinth = array => {
         content += "<tr>";
         for (let j = 0; j < array[i].length; j++) {
             content += "<td>";
-            content += "<img src='images/" + array[i][j] + ".png' />"
+            content += "<img src='images/" + array[i][j].picture + ".png' />"
             if (i === nbRow - 1 && j === nbCol - 1) {
                 let chara2Line = 25 + 100 * i;
                 let chara2Col = 25 + 100 * j;
                 content += "<img class='character1' src='images/1pixel.png' />"
+                content += "<img class='speech-bubble hidden' src='images/pixel-speech-bubble.gif' />"
             }
             if (i === posPlayer[0] && j === posPlayer[1]) {
                 let chara1Line = 25 + 100 * posPlayer[0];
@@ -73,11 +70,13 @@ const displayLabyrinth = array => {
     }
     content += "</table>";
     myGame.innerHTML = content;
+    displaySpeechBubble();
+
 }
 displayLabyrinth(gameArray);
 
 const getCell = (i, j) => {
-    return tab[i][j];
+    return gameArray[i][j];
 }
 
 document.addEventListener("keyup", function (event) {
@@ -87,43 +86,36 @@ document.addEventListener("keyup", function (event) {
     switch (event.key) {
         case "ArrowDown":
             if (linePlayer < nbRow - 1) {
-                linePlayer += 1;
-                /*
                 if (getCell(linePlayer, colPlayer).bot) {
                     linePlayer += 1;
-                }*/
+                    verifIfAtEnd(linePlayer, colPlayer);
+                }
             }
             console.log(linePlayer)
             break;
         case "ArrowUp":
             if (linePlayer > 0) {
-                /*
                 if (getCell(linePlayer, colPlayer).top) {
                     linePlayer -= 1;
-                }*/
-                linePlayer -= 1;
-
+                    verifIfAtEnd(linePlayer, colPlayer);
+                }
             }
             break;
         case "ArrowLeft":
             console.log(colPlayer)
             if (colPlayer > 0) {
-                /*
                 if (getCell(linePlayer, colPlayer).left) {
                     colPlayer -= 1;
-                }*/
-                colPlayer -= 1;
+                    verifIfAtEnd(linePlayer, colPlayer);
+                }
             }
             break;
         case "ArrowRight":
             if (colPlayer < nbCol - 1) {
-                colPlayer += 1;
-
-                /*
                 if (getCell(linePlayer, colPlayer).right) {
                     colPlayer += 1;
-                }*/
-
+                    verifIfAtEnd(linePlayer, colPlayer);
+                }
             }
             break;
         default:
@@ -134,3 +126,27 @@ document.addEventListener("keyup", function (event) {
 
     console.log(event.key);
 });
+
+const verifIfAtEnd = (posPlayerLine, posPlayerCol) => {
+    if (posPlayerLine === (nbRow - 1) && posPlayerCol === (nbCol - 1)) {
+        let content = "";
+        content += "<p class='text-alert'>Well done ! Next level : " + (niveauEnCours + 1) + " ?</p>";
+        content += "<button class='btn btn-primary' onClick='launchNextLevel()'> Next </button>"
+        alert.innerHTML = content;
+        alert.classList.remove('d-none');
+    }
+}
+function displaySpeechBubble() {
+    if (posPlayer[0] === nbRow - 1 && posPlayer[1] === nbCol - 1) {
+        const speechBubble = document.querySelector('.speech-bubble');
+        console.log(speechBubble);
+        if (speechBubble) {
+            speechBubble.style.display = 'block';
+        }
+    } else {
+        const speechBubble = document.querySelector('.speech-bubble');
+        if (speechBubble) {
+            speechBubble.style.display = 'none';
+        }
+    }
+}
